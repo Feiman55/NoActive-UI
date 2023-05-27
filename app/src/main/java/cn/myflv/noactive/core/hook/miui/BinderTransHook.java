3c9429b415e92422ca271fa8d1ed3fd49595c178
Feiman55 <112330327+Feiman55@users.noreply.github.com>
2023-05-27 14:17:09 +0800

    Update BinderTransHook.java

--- app/src/main/java/cn/myflv/noactive/core/hook/miui/BinderTransHook.java
+++ app/src/main/java/cn/myflv/noactive/core/hook/miui/BinderTransHook.java
@@ -1,98 +1,156 @@
 package cn.myflv.noactive.core.hook.miui;
 
+import java.util.Map;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 import cn.myflv.noactive.constant.ClassConstants;
+
 import cn.myflv.noactive.constant.MethodConstants;
+
 import cn.myflv.noactive.core.handler.FreezerHandler;
+
 import cn.myflv.noactive.core.hook.base.AbstractMethodHook;
+
 import cn.myflv.noactive.core.hook.base.MethodHook;
+
 import de.robv.android.xposed.XC_MethodHook;
 
 /**
+
  * Binder通信Hook.
+
  */
+
 public class BinderTransHook extends MethodHook {
-    
+
+    /**
+
+     * 上一次解冻时间Map.
+
+     */
+
     private final static Map<Integer, Long> lastThawMap = new ConcurrentHashMap<>();
 
-    private final static String REASON = "received sync binder";
-    //定义的ASON是对应异步解冻测试的，可以删除
-    private final static String ASON = "received oneway binder";
+    private final static String SYNC_REASON = "received sync binder";
+
+    private final static String ASYNC_REASON = "received async binder";
+
+    private final static int ASYNC_INTERVAL = 60 * 1000;
+
     /**
+
      * 应用切换Hook
+
      */
+
     private final FreezerHandler freezerHandler;
 
     public BinderTransHook(ClassLoader classLoader, FreezerHandler freezerHandler) {
+
         super(classLoader);
+
         this.freezerHandler = freezerHandler;
+
     }
 
     @Override
+
     public String getTargetClass() {
+
         return ClassConstants.GreezeManagerService;
+
     }
 
     @Override
+
     public String getTargetMethod() {
+
         return MethodConstants.reportBinderTrans;
+
     }
 
     @Override
+
     public Object[] getTargetParam() {
+
         return new Object[]{int.class, int.class, int.class, int.class, int.class, boolean.class, long.class, int.class};
+
     }
 
     @Override
+
     public XC_MethodHook getTargetHook() {
+
         return new AbstractMethodHook() {
+
             @Override
+
             protected void beforeMethod(MethodHookParam param) throws Throwable {
+
                 Object[] args = param.args;
+
                 int uid = (int) args[0];
-                // 是否异步
-                boolean isOneway = (boolean) args[5];
-                if (isOneway) {
-                    
+
+                //获取当前时间戳
+
                 long currentTime = System.currentTimeMillis();
 
-                //获取上次解冻时间戳，如果没有解冻就是0
+                // 是否异步
 
-                Long lastThawTime = lastThawMap.computeIfAbsent(uid, k -> 0L);
+                boolean isOneway = (boolean) args[5];
 
-                //如果当前时间-上次解冻时间，小于60秒就return返回
+                if (isOneway) {
 
-                if (currentTime - lastThawTime < 60 * 1000 ){
+                    //获取上次解冻时间戳，如果没有解冻就是0
 
-                return;
+                    Long lastThawTime = lastThawMap.computeIfAbsent(uid, k -> 0L);
+
+                    //如果当前时间-上次解冻时间，小于60秒就return返回
+
+                    if (currentTime - lastThawTime < ASYNC_INTERVAL) {
+
+                        return;
+
+                    }
 
                 }
 
                 //存入当前时间
 
                 lastThawMap.put(uid, currentTime);
-                    // 异步不处理，下边两行是异步解冻可以删除，目前是测试用的
-                   // Log.i("这是异步binder解冻处理");
-                    freezerHandler.temporaryUnfreezeIfNeed(uid, ASON);
-                    return;
-                }
-                freezerHandler.temporaryUnfreezeIfNeed(uid, REASON);
+
+                freezerHandler.temporaryUnfreezeIfNeed(uid, isOneway ? ASYNC_REASON : SYNC_REASON);
+
             }
+
         };
+
     }
 
     @Override
+
     public int getMinVersion() {
+
         return ANY_VERSION;
+
     }
 
     @Override
+
     public String successLog() {
+
         return "Perfect Freezer";
+
     }
 
     @Override
+
     public boolean isIgnoreError() {
+
         return true;
+
     }
 
 }
+
