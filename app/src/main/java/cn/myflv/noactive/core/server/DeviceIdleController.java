d83ceb8bf69dbbd4d7eaab7b2d946fd0aa9bf4da
Feiman55 <112330327+Feiman55@users.noreply.github.com>
2023-05-30 08:54:42 +0800

    Update DeviceIdleController.java

--- app/src/main/java/cn/myflv/noactive/core/server/DeviceIdleController.java
+++ app/src/main/java/cn/myflv/noactive/core/server/DeviceIdleController.java
@@ -43,7 +43,7 @@ public class DeviceIdleController {
             curState = getCurState();
         }
         idle = true;
-        Log.d("deep doze success");
+        Log.i("deep doze success");
     }
 
 
